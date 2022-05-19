694258e2ec824d3e659f67bd5bfe896756a8473b
Peter Tran <pettran866@gmail.com>
2022-05-19 15:10:43 -0700

    added Manager object, constructor, and methods

--- __test__/Manager.test.js
+++ __test__/Manager.test.js
@@ -1,40 +1,40 @@
 const Manager = require('../lib/Manager')
 
 test('creates an Manager object', () => {
-  const Manager = new Manager('Dave','1','dave@fakemail.com', '1')
+  const manager = new Manager('Dave','1','dave@fakemail.com', '1')
 
-  expect(Manager.name).toBe('Dave');
-  expect(Manager.id).toBe('1');
-  expect(Manager.email).toBe('dave@fakemail.com');
-  expect(Manager.officeNumber).toBe('1');
+  expect(manager.name).toBe('Dave');
+  expect(manager.id).toBe('1');
+  expect(manager.email).toBe('dave@fakemail.com');
+  expect(manager.officeNumber).toBe('1');
 });
 
 test("gets Manager's name", () => {
-  const Manager = new Manager('Dave','1','dave@fakemail.com', '1')
+  const manager = new Manager('Dave','1','dave@fakemail.com', '1')
   
-  expect(Manager.getName()).toEqual('Dave');
+  expect(manager.getName()).toEqual('Dave');
 });
 
 test("gets Manager's id", () => {
-  const Manager = new Manager('Dave','1','dave@fakemail.com', '1')
+  const manager = new Manager('Dave','1','dave@fakemail.com', '1')
   
-  expect(Manager.getId()).toEqual('1');
+  expect(manager.getId()).toEqual('1');
 })
 
 test("gets Manager's id", () => {
-  const Manager = new Manager('Dave','1','dave@fakemail.com', '1')
+  const manager = new Manager('Dave','1','dave@fakemail.com', '1')
   
-  expect(Manager.getEmail()).toEqual('dave@fakemail.com');
+  expect(manager.getEmail()).toEqual('dave@fakemail.com');
 })
 
 test("gets Manager's office number", () => {
-  const Manager = new Manager('Dave','1','dave@fakemail.com', '1')
+  const manager = new Manager('Dave','1','dave@fakemail.com', '1')
   
-  expect(Manager.getOfficeNumber()).toEqual('1');
+  expect(manager.getOfficeNumber()).toEqual('1');
 });
 
-test("gets Manager's id", () => {
-  const Manager = new Manager('Dave','1','dave@fakemail.com', '1')
+test("gets Manager's role", () => {
+  const manager = new Manager('Dave','1','dave@fakemail.com', '1')
   
-  expect(Manager.getRole()).toEqual('Manager');
+  expect(manager.getRole()).toEqual('Manager');
 })
