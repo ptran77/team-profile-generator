2baaa23f25a8861dcb69be767cba4feace71c8da
Peter Tran <pettran866@gmail.com>
2022-05-21 14:00:54 -0700

    added Employee prompts

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 
 // team members array
-const teamMembers = []
+const teamMembers = [];
 
 // function to validate emails
 const validateEmail = email => {
@@ -14,16 +14,17 @@ const validateEmail = email => {
   return emailFormat.test(email);
 }
 
+// function to get Manager info and store into team member array
 const getManager = () => {
-  inquirer.prompt([
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
-      message: "What is your manager's name? (Required)",
+      message: "What is the manager's name? (Required)",
       validate: nameInput => {
         if (nameInput) return true;
         else {
-          console.log("You need to enter your manager's name.");
+          console.log("You need to enter the manager's name.");
           return false;
         }
       }
@@ -31,11 +32,11 @@ const getManager = () => {
     {
       type: 'input',
       name: 'id',
-      message: "What is your manager's id? (Required)",
+      message: "What is the manager's id? (Required)",
       validate: idInput => {
         if(idInput) return true;
         else {
-          console.log("You need to enter your manager's id.");
+          console.log("You need to enter the manager's id.");
           return false;
         }
       }
@@ -43,11 +44,11 @@ const getManager = () => {
     {
       type: 'input',
       name: 'email',
-      message: "What is your manager's email? (Required)",
+      message: "What is the manager's email? (Required)",
       validate: emailInput => {
         if(validateEmail(emailInput)) return true;
         else {
-          console.log("You need to enter your manager's email.");
+          console.log("You need to enter the manager's email.");
           return false;
         }
       }
@@ -55,23 +56,122 @@ const getManager = () => {
     {
       type: 'input',
       name: 'officeNumber',
-      message: "What is your manager's office number? (Required)",
+      message: "What is the manager's office number? (Required)",
       validate: officeNumber => {
         // checking that the input is an actual number
         if(!isNaN(officeNumber)) return true;
         else {
-          console.log("You need to put your manager's office number.");
+          console.log("You need to put the manager's office number.");
           return false;
         }
       }
     }
   ])
   .then(managerData => {
-    // creating Manager object and addd to team members array
+    // creating Manager object and add to team members array
     const {name, id, email, officeNumber} = managerData;
     const manager = new Manager(name, id, email, officeNumber);
     teamMembers.push(manager);
   })
 };
 
-getManager();
+
+// function to get all employees and get their profile into the team member array
+const getEmployees = () => {
+  console.log(`
+===================
+Adding Team Members
+==================
+`);
+  return inquirer.prompt([
+    {
+      type: 'list',
+      name: 'role',
+      message: "Who would do you want to add?",
+      choices: ['Engineer', 'Intern', 'None']
+      
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the member's name? (Required)",
+      when: (input) => input.role !== 'None',
+      validate: nameInput => {
+        if (nameInput) return true;
+        else {
+          console.log("You need to enter the team member's name.");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is your member's id? (Required)",
+      when: (input) => input.role !== 'None',
+      validate: idInput => {
+        if (idInput) return true;
+        else {
+          console.log("You need to enter the team member's id.");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the member's email? (Required)",
+      when: (input) => input.role !== 'None',
+      validate: emailInput => {
+        if(validateEmail(emailInput)) return true;
+        else {
+          console.log("You need to enter the member's email.");
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: "What is the engineer's github? (Required)",
+      when: (input) => input.role === 'Engineer',
+      validate: githubInput => {
+        if (githubInput) return true;
+        else {
+          console.log("You need to enter the engineer's github.")
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: "What is the intern's school? (Required)",
+      when: (input) => input.role === 'Intern',
+      validate: schoolInput => {
+        if (schoolInput) return true;
+        else {
+          console.log("You need to enter the intern's school.")
+          return false;
+        }
+      }
+    }
+  ])
+  .then(memberData => {
+    const {role} = memberData;
+    if(role === 'Engineer') {
+      const {name, id, email, github} = memberData;
+      const engineer = new Engineer(name, id, email, github);
+      teamMembers.push(engineer);
+    }
+    else if(role === 'Intern') {
+      const {name, id, email, school} = memberData;
+      const intern = new Intern(name, id, email, school);
+      teamMembers.push(intern);
+    }
+    if(role !== 'None') getMembers();
+    else return teamMembers;
+  });
+}
+
+getManager().then(getEmployees);
